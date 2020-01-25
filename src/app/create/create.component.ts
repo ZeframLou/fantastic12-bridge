a20beef26d38a17431161d5eda16733d2bc76543
Zefram Lou <zeframlou@gmail.com>
2020-01-24 19:07:42 -0800

    fixed create component

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -16,8 +16,10 @@ export class CreateComponent extends Web3Enabled implements OnInit {
   txHash: string;
 
   FACTORY_ADDRESS: string;
-  DEFAULT_WITHDRAW_LIMIT: BigNumber;
-  DEFAULT_CONSENSUS_THRESHOLD: BigNumber;
+  DEFAULT_SHARE_NAME: string;
+  DEFAULT_SHARE_SYMBOL: string;
+  DEFAULT_SHARE_DECIMALS: number;
+  DEFAULT_SUMMONER_SHARES: BigNumber;
 
   constructor(@Inject(WEB3) web3: Web3, private route: ActivatedRoute) {
     super(web3);
@@ -26,8 +28,11 @@ export class CreateComponent extends Web3Enabled implements OnInit {
     this.txSubmitted = false;
     this.txHash = "";
     this.FACTORY_ADDRESS = "0x5339CA2b8148C5803236D8592a98411c4Ae881f8";
-    this.DEFAULT_WITHDRAW_LIMIT = new BigNumber(1000 * 1e18);
-    this.DEFAULT_CONSENSUS_THRESHOLD = new BigNumber(0.75 * 1e18);
+
+    this.DEFAULT_SHARE_NAME = "Fantastic12 Squad Share";
+    this.DEFAULT_SHARE_SYMBOL = "F12-SHARE";
+    this.DEFAULT_SHARE_DECIMALS = 18;
+    this.DEFAULT_SUMMONER_SHARES = new BigNumber(100 * 1e18);
   }
 
   ngOnInit() {
@@ -38,7 +43,7 @@ export class CreateComponent extends Web3Enabled implements OnInit {
         const factory = new this.web3.eth.Contract(abi, this.FACTORY_ADDRESS);
 
         // Call createSquad()
-        this.sendTx(factory.methods.createSquad(this.summoner, this.DEFAULT_WITHDRAW_LIMIT.toFixed(), this.DEFAULT_CONSENSUS_THRESHOLD.toFixed()),
+        this.sendTx(factory.methods.createSquad(this.summoner, this.DEFAULT_SHARE_NAME, this.DEFAULT_SHARE_SYMBOL, this.DEFAULT_SHARE_DECIMALS, this.DEFAULT_SUMMONER_SHARES.toFixed()),
           (txHash) => {
             this.txSubmitted = true;
             this.txHash = txHash;
